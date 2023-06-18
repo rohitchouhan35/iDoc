53181affd6e35cd4bc7aee5b6d2c9e6dba2d2228
rohit <rohitchouhan3512@gmail.com>
2023-06-18 22:47:55 +0530

    Added Authorization

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -9,9 +9,8 @@ export const authOptions = {
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       NEXTAUTH_URL: 'http://localhost:3000/api/auth/callback/google'
-    })
+    }),
   ],
-
 }
 
 export default NextAuth(authOptions)
